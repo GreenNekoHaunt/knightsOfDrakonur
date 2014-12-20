c981eded88660568581921587f4baf9944f07ff8
GreenyNeko <neubert.pascal@yahoo.de>
2014-12-20 20:28:06 +0100

    Implemented the menu and some scenes/activities.
    Implements the menu and some scenes/activities to work with.

--- src/main/java/src/LoadResources.java
+++ src/main/java/src/LoadResources.java
@@ -20,11 +20,6 @@ public class LoadResources extends Screen
     public void update(float deltaTime)
     {
         Graphics graphics = game.getGraphics();
-        Assets.unused = graphics.newImage("gfx/skills/unused.png", ImageFormat.RGB565);
-        Assets.ui_skillSlot = graphics.newImage("gfx/ui/skillSlot.png", ImageFormat.ARGB4444);
-        Assets.ui_skillSlotLeft = graphics.newImage("gfx/ui/skillSlotLeft.png", ImageFormat.ARGB4444);
-        Assets.ui_skillSlotRight = graphics.newImage("gfx/ui/skillSlotRight.png", ImageFormat.ARGB4444);
-        Assets.skill_fireball = graphics.newImage("gfx/skills/fireball.png", ImageFormat.RGB565);
 
         game.setScreen(new TitleScreen(game));
     }
--- src/main/java/src/SettingsScreen.java
+++ src/main/java/src/SettingsScreen.java
@@ -4,27 +4,21 @@ package src;
  * Created by GreenyNeko on 11.12.2014.
  */
 
-import java.util.List;
-import java.util.Locale;
-
-import android.content.res.Configuration;
-import android.content.Context;
 import android.graphics.Paint;
-import android.text.TextPaint;
-import android.widget.Toast;
 
 import com.kod.knightsofdrakonur.framework.Game;
 import com.kod.knightsofdrakonur.framework.Graphics;
-import com.kod.knightsofdrakonur.framework.Screen;
 import com.kod.knightsofdrakonur.framework.Input.TouchEvent;
+import com.kod.knightsofdrakonur.framework.Screen;
+
+import java.util.List;
+import java.util.Locale;
 
 import util.Math;
 
-public class TitleScreen extends Screen
+public class SettingsScreen extends Screen
 {
-    public int ticks = 0;
-
-    public TitleScreen(Game game)
+    public SettingsScreen(Game game)
     {
         super(game);
     }
@@ -54,18 +48,6 @@ public class TitleScreen extends Screen
         int screenH = game.getGraphics().getHeight(), screenW = game.getGraphics().getWidth();
         Graphics graphics = game.getGraphics();
         graphics.clearScreen(0);
-
-        Paint textStyle = new Paint();
-        textStyle.setARGB(250, 250, 250, 0);
-        textStyle.setTextAlign(Paint.Align.CENTER);
-        textStyle.setTextSize(32.0f);
-        textStyle.setTextLocale(Locale.ENGLISH);
-        graphics.drawString(String.valueOf(ticks), 200, 50, textStyle);
-        if(ticks % 100 < 50)
-        {
-            graphics.drawString("Tap the screen!", (int) (screenW * 0.5), (int) (screenH * 0.9), textStyle);
-        }
-        ticks++;
     }
 
     @Override
